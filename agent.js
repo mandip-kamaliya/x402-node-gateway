6e32b917bbcde863293a4f32b8a1ede79220641d
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-25 11:40:29 +0530

    added configuration

--- agent.js
+++ agent.js
@@ -1,3 +1,6 @@
 require('dotenv').config();
 const axios = require('axios');
 const { ethers } = require('ethers');
+
+const provider = new ethers.JsonRpcProvider("https://evm-t3.cronos.org");
+const wallet = new ethers.Wallet(process.env.AGENT_PRIVATE_KEY, provider);
