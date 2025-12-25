c799216ccc46c52ef43f56d7d261e1f9562105be
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-25 11:36:55 +0530

    added configuration

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const { ethers } = require('ethers');
 
 const app = express();
 const PORT = 3000;
+
+// CONFIGURATION
+const PROVIDER_URL = "https://evm-t3.cronos.org"; // Cronos Testnet
+const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
+const MERCHANT_WALLET = process.env.MERCHANT_WALLET;
+const PRICE = "0.01";
